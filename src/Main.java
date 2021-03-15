642e8d443a422affed5d24e6ce271991365ae0a0
Zubareva-Anna <a84.zubareva@gmail.com>
2021-03-15 23:12:40 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double weight = 68.80;
-        int miles = service.calculate(weight);
-        System.out.println(miles);
+        int index = service.calculate(weight);
+        System.out.println(index);
     }
 }
